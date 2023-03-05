18ba67c4c4b3d958894d06ee120c7538e48a9a27
Alon Landmann <rafael.landmann@gmail.com>
2023-03-05 23:59:31 +0100

    [add] start and strength settings

--- components/Home.js
+++ components/Home.js
@@ -3,6 +3,8 @@ import { useState } from 'react'
 
 export default function Home() {
   const [symbol, setSymbol] = useState('x')
+  const [start, setStart] = useState('x')
+  const [strength, setStrength] = useState('hard')
 
   return (
     <div className={css.container}>
@@ -22,7 +24,48 @@ export default function Home() {
           o
         </div>
       </div>
-
+      <h2 className={css.heading}>Who starts?</h2>
+      <div className={css.symbols}>
+        <div
+          className={`${start === 'x' ? css.cross : css.off}`}
+          onClick={() => { setStart('x') }}
+        >
+          x
+        </div>
+        <div
+          className={`${start === 'o' ? css.circle : css.off}`}
+          onClick={() => { setStart('o') }}
+        >
+          o
+        </div>
+      </div>
+      <h2>AI Strength</h2>
+      <div className={css.strengthOptions}>
+        <div
+          className={`${strength === 'easy' ? css.selectedStrength : css.notSelectedStrength}`}
+          onClick={() => { setStrength('easy') }}
+        >
+          easy
+        </div>
+        <div
+          className={`${strength === 'medium' ? css.selectedStrength : css.notSelectedStrength}`}
+          onClick={() => { setStrength('medium') }}
+        >
+          medium
+        </div>
+        <div
+          className={`${strength === 'hard' ? css.selectedStrength : css.notSelectedStrength}`}
+          onClick={() => { setStrength('hard') }}
+        >
+          hard
+        </div>
+        <div
+          className={`${strength === 'tornado' ? css.selectedStrength : css.notSelectedStrength}`}
+          onClick={() => { setStrength('tornado') }}
+        >
+          tornado
+        </div>
+      </div>
     </div>
   )
 }
